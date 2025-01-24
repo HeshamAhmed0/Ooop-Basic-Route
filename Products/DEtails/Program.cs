2ba62411dc5a8e1765fe0405327d2c5186a8b071
Hesham Mathana <heshammathana0@gmail.com>
2025-01-24 16:30:49 +0200

    The First App For Oop Fundamentals

---
+++ Products/DEtails/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+namespace DEtails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name= "Test";
+            product.Id= 1;
+        }
+    }
+}
