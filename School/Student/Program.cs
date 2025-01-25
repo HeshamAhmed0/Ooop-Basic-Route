891b11c549c020d6b26b334b04b90a6195823cd1
Hesham Mathana <heshammathana0@gmail.com>
2025-01-25 04:18:55 +0200

    This App For Students this Take From Student Name ,  Id , Genger (Enum) and Branch(Enum)

---
+++ School/Student/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StDetails stDetails = new StDetails();
+            bool IsParsed;
+            int StId;
+            do
+            {
+                Console.Write("Enter Your Id : ");
+                IsParsed = int.TryParse(Console.ReadLine(), out StId);
+            } while (!IsParsed);
+            stDetails.Id = StId;
+            Console.Clear();
+
+
+            string Name;
+            do
+            {
+                Console.Write("Enter Your Name : ");
+                 Name = Console.ReadLine();
+                if (string.IsNullOrEmpty(Name) || !Name.All(char.IsLetter))
+                {
+                    Console.Write("Enter Valid Name : ");
+                    Name = Console.ReadLine();
+                }
+
+            } while (!IsParsed);
+            stDetails.Name = Name;
+            Console.Clear();
+
+
+
+            Gender Gend;
+            do
+            {
+                Console.Write("Enter Your Gender : ");
+                IsParsed = Enum.TryParse<Gender>(Console.ReadLine(), out Gend);
+            } while (!IsParsed);
+            stDetails.Gender= Gend;
+            Console.Clear();
+
+
+
+            Branch branch;
+            do
+            {
+                Console.Write("Enter Your Branch : ");
+                IsParsed =Enum.TryParse<Branch>(Console.ReadLine(),out branch);
+            } while (!IsParsed);
+            stDetails.Branch= branch;
+            Console.Clear();
+
+            Console.WriteLine($"Hello {stDetails.Name} \n Your Branch is {stDetails.Branch}");
+        }
+    }
+}
---
+++ School/Student/StDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class StDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public Branch Branch { get; set; }
+    }
+}
