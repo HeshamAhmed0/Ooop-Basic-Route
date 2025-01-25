0aea804f2b4d8a52354116fddc25cce56de24e26
Hesham Mathana <heshammathana0@gmail.com>
2025-01-25 02:11:32 +0200

    The First  Applied For Enum

---
+++ Producs/Human/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Human
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ex.1
+            Human01 h = new Human01();
+            h = Human01.Female;
+            Console.WriteLine(h);
+            h = (Human01)2;
+            Console.WriteLine(h); 
+            #endregion
+        }
+    }
+}
