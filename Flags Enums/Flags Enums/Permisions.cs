88c04a6a66653257847d7fd9f9e147a776bec629
Hesham Mathana <heshammathana0@gmail.com>
2025-01-27 02:09:02 +0200

    An Example For Flag Enum Property

---
+++ Flags Enums/Flags Enums/Permisions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flags_Enums
+{
+    internal enum Permisions : byte
+    {
+        None = 0,
+        Write=1,
+        Delete =2,
+        Read =4,
+    }
+}
---
+++ Flags Enums/Flags Enums/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Flags_Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Flags Enum
+            Human humans = new Human();
+            //humans.Id = 1;
+            //humans.Name = "test";
+            //humans.Properties = Properties.Delete;
+            humans.Permisions = (Permisions)3;
+            //Console.WriteLine(humans.Properties);
+            //humans.Properties = humans.Properties ^ Properties.read;
+            //Console.WriteLine(humans.Properties); 
+
+            // Check for permission
+            if ((humans.Permisions & Permisions.Read) == Permisions.Read)
+            {
+                Console.WriteLine("This Human Has Read Permission ");
+            }
+            else
+            {
+                Console.WriteLine("This Human Not Has Read Property");
+                humans.Permisions ^= Permisions.Read;
+                Console.WriteLine("Operation Add Property Read Done Succcesful");
+                Console.WriteLine(humans.Permisions);
+
+            }
+            #endregion
+        }
+    }
+}
