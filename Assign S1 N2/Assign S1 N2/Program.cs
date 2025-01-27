3cacf4e50c1ddeddd2a1712da8afbb9dc1a88805
Hesham Mathana <heshammathana0@gmail.com>
2025-01-27 04:50:10 +0200

    New App in Enum

---
+++ Assign S1 N2/Assign S1 N2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assign_S1_N2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Season Name : ");
+            string input=Console.ReadLine();
+            Season SeasionName;  Enum.TryParse(input, true, out SeasionName);
+            if (SeasionName == Season.Spring)
+            {
+                Console.WriteLine("Spring: March to May");
+            } else if (SeasionName==Season.Winter)
+            {
+                Console.WriteLine("Winter: December to February");
+
+            }
+            else if (SeasionName == Season.Autumn)
+            {
+                Console.WriteLine("Autumn: September to November");
+
+            }
+            else if(SeasionName == Season.Summer)
+            {
+                Console.WriteLine("Summer: June to August");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid season.");
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Assign S1 N2/Assign S1 N2/Season.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_S1_N2
+{
+    [Flags]
+    internal enum Season
+    {
+        none=0,
+        Spring=1,
+        Summer=2,
+        Autumn=4, 
+        Winter=8,
+    }
+}
