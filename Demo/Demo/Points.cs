46f1731ca681fbdf0f3d392f9e3eadaf76f54579
Hesham Mathana <heshammathana0@gmail.com>
2025-01-28 00:05:06 +0200

    Apply For Struct And Edit in built in Function ToString

---
+++ Demo/Demo/Points.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal struct Points
+    {
+        #region Attributes
+        public int x;
+        public int y;
+        #endregion
+
+        #region Constractors
+        public Points()
+        {
+            x = 10;
+            y = 10;
+        }
+        public Points(int x, int y) { 
+        this.x = x;
+            this.y = y;
+        }
+        public Points(int Number)
+        {
+            this.x = Number;
+            this.y = Number;
+        }
+
+        public override string ToString()
+        {
+            return $"{x},{y}";
+        }
+
+        #endregion
+
+    }
+}
---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Points P1;
+            // CLR Will Allocate 8 un initialized bytes in stack with garpage 
+            P1= new Points();
+            // CLR Will Call Parameter Less Conestactor And Make X=0 And Y=0
+            P1 = new(100);
+            Console.WriteLine(P1.y);
+            Console.WriteLine(P1);
+            //will Call Object And Object Will Call WriteLine And WriteLine Will Call ToString
+            Console.WriteLine(P1.ToString);
+        }
+    }
+}
