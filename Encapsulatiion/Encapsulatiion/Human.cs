c12702b7408df38e3090cbacb87cafc902454bf3
Hesham Mathana <heshammathana0@gmail.com>
2025-01-28 02:10:42 +0200

    Apply For Encapsulation

---
+++ Encapsulatiion/Encapsulatiion/Human.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulatiion
+{
+    internal struct Human
+    {
+        #region Attriputes
+        private int Id;
+        private string? EmName;
+        #endregion
+
+        #region Getter And Setter 
+        //public string? GetName()
+        //{
+        //    return EmName;
+        //}
+
+        //public void SetName(String? name)
+        //{
+        //    EmName = name?.Length > 10 ? name.Substring(0, 10) : name;
+        //} 
+        #endregion
+
+
+        #region Property
+        public int EmId
+        {
+            get { return Id; }
+            set { Id = value < 200 ? 200 : value; }
+        }
+        #endregion
+
+
+    }
+}
---
+++ Encapsulatiion/Encapsulatiion/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Encapsulatiion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+Human human = new Human();
+            //human.GetName();
+            //human.SetName ("Hesham");
+            //human.GetName();
+            //Console.WriteLine(human.GetName());
+            human.EmId= 1111;
+            Console.WriteLine(human.EmId);
+        }
+    }
+}
+// Encapsulation 
+// Not Allow You To Deal With Attributes 
+// This Allow You to deal with functions and functions alow you to deal with attriputes
