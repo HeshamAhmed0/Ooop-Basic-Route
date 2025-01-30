e1f3c8f024d1345f559b517cf9ea58a26de416db
Hesham Mathana <heshammathana0@gmail.com>
2025-01-30 05:23:45 +0200

    Apply For Chaining

---
+++ ApplyForChaining/ApplyForChaining/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyForChaining
+{
+    internal class Person
+    {
+        #region Attributes
+        private int id;
+        private string? name;
+        public int Age;
+
+
+        #endregion
+
+        #region Constructor
+        public Person(int id, string? name, int Age)
+        {
+            this.id = id;
+            this.name = name;
+            this.Age = Age;
+        }
+        public Person(int Age) : this(10, "Hesham", Age) { }
+        public Person(int Age, string Name) : this(20, Name, Age) { }
+        #endregion
+
+        public override string ToString()
+        {
+            return $" Id : {id} , Name : {name} , Age : {Age}";
+        }
+
+    }
+}
---
+++ ApplyForChaining/ApplyForChaining/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ApplyForChaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(10);
+            Console.WriteLine(p1);
+        }
+    }
+}
