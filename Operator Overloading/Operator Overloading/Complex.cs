a3de0717bcf14357681bbf8109004d929bcfc6d4
Hesham Mathana <heshammathana0@gmail.com>
2025-02-01 05:12:43 +0200

    Aplly For Operator Overloading

---
+++ Operator Overloading/Operator Overloading/Complex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Img { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i";
+        }
+
+        public static Complex operator + (Complex left, Complex right) 
+        {
+            //Complex result = new Complex()
+            //{
+            //    Real = left.Real + right.Real,
+            //    Img = left.Img + right.Img,
+
+            //};
+            //return result;
+
+
+
+            // Another Way For New Complex 
+
+            return new Complex
+            {
+                Real = (left?.Real??0) + (right?.Real ?? 0),
+                Img = (left?.Img??0) + (right?.Img??0),
+            };
+
+        }
+    }
+}
---
+++ Operator Overloading/Operator Overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Operator_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex C1=new Complex();
+            // will initialize the default value 
+            C1.Real = 10;
+            C1.Img = 5;
+            // will initialize Real =10 and Img =5
+            Console.WriteLine(C1);
+
+            // will make the same steps
+            Complex C2=new Complex();
+            C2.Real = 10;
+            C2.Img = 5;
+            Console.WriteLine(C2);
+
+
+            Complex C3=new Complex();
+            C3 = C1 + C2;
+            Console.WriteLine(C3);
+
+        }
+    }
+}
