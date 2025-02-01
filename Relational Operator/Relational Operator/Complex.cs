e173b345b9bc9aebc38f1470695f9f98d6238023
Hesham Mathana <heshammathana0@gmail.com>
2025-02-01 06:10:01 +0200

    Apply For Relational Operator

---
+++ Relational Operator/Relational Operator/Complex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relational_Operator
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Img { get; set; }
+
+        public static bool operator >(Complex C1 ,Complex C2)
+        {
+            if (C1?.Real==C2?.Real)
+            {
+                return C1?.Img>C2?.Img;
+            }
+            else
+            {
+               return C1?.Real > C2?.Real;
+            }
+        }
+
+        public static bool operator <(Complex C1, Complex C2)
+        {
+            if (C1?.Real == C2?.Real)
+            {
+                return C1?.Img < C2?.Img;
+            }
+            else
+            {
+                return C1?.Real < C2?.Real;
+            }
+        }
+
+    }
+}
---
+++ Relational Operator/Relational Operator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Relational_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1= new Complex();
+            Complex c2= new Complex();
+            c1.Real = 10;
+            c2.Real = 20;
+            c1.Img = 5;
+            c2.Img = 6;
+            string message = c1 > c2 ? "C1 > C2 " : "C1 < C2";
+            Console.WriteLine(message);
+        }
+    }
+}
