b42c04dc5a5e9b6953fd47b525bb61bf4d119c42
Hesham Mathana <heshammathana0@gmail.com>
2025-02-01 05:52:20 +0200

    Applying For Unary Operator

---
+++ Unary Operator Overloading/Unary Operator Overloading/Complex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unary_Operator_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+
+        public int Img { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i";
+        }
+
+        public static Complex operator ++(Complex C) 
+        {
+            return new Complex
+            {
+                Real = (C?.Real ?? 0) + 1,
+                Img = C.Img
+            };
+        }
+    }
+}
---
+++ Unary Operator Overloading/Unary Operator Overloading/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Unary_Operator_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+Complex complex = new Complex();
+            complex.Real = 10;
+            complex.Img = 5;
+            Console.WriteLine(complex++);
+            Console.WriteLine(++complex);
+
+        }
+    }
+}
