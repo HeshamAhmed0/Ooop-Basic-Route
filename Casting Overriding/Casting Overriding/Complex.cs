8cfe1d00e24bf69a6877efad4e28f10e2fb358c4
Hesham Mathana <heshammathana0@gmail.com>
2025-02-01 15:04:46 +0200

    Apply For Casting Overloading

---
+++ Casting Overriding/Casting Overriding/Complex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting_Overriding
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Img { get; set; }
+
+        public static explicit operator string (Complex complex)
+        {
+            return $"{complex?.Real ?? 0} + {complex?.Img ?? 0}";
+        }
+        public static implicit operator int(Complex complex)
+        { 
+        return complex?.Real??0 ;
+        }
+
+    }
+}
---
+++ Casting Overriding/Casting Overriding/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Casting_Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+Complex C1 = new Complex();
+            int x = C1;
+            Console.WriteLine(x);    // will return default value => 0
+            C1.Real = 10;
+            x= C1;
+            Console.WriteLine(x);    // will return Real Part => 10
+        }
+    }
+}
