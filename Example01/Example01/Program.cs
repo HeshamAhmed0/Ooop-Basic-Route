d90cd2e78a97edcc57484344e929604b9735ab3d
Hesham Mathana <heshammathana0@gmail.com>
2025-02-03 16:25:05 +0200

    Example01 For Interface

---
+++ Example01/Example01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Example01
+{
+    internal class Program
+    {
+        public static void GetDetails(ISeries ISeriesObj)
+        {
+            if (ISeriesObj is not null)
+            {
+                for (int i=1;i<=5;i++)
+                {
+                    Console.WriteLine(ISeriesObj.Current);
+                    ISeriesObj.GetNext();
+                }
+                ISeriesObj.Reset();
+            }
+        }
+        static void Main(string[] args)
+        {
+        TypeB typeAObj =new TypeB();
+            GetDetails(typeAObj);
+
+        }
+    }
+}
