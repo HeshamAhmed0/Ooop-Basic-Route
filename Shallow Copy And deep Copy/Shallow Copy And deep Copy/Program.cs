c8adc078bb463a6321ff27df34736a62be1dcd56
Hesham Mathana <heshammathana0@gmail.com>
2025-02-05 22:47:13 +0200

    Apply For Shallow Copy And Deep Copy Immutable Reference Type

--- Shallow Copy And deep Copy/Shallow Copy And deep Copy/Program.cs
+++ Shallow Copy And deep Copy/Shallow Copy And deep Copy/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Shallow Copy And Reference Copy Value Type
+            #region Shallow Copy
+            //int[] Arr01 = { 1, 2, 3, };
+            //int[] Arr02 = new int[2];
+            //Arr02 = Arr01;
+            // In This Case i Make Shallo Coppy 
+            // i take instance of Arr01 and make it in insTANCE aRR02
+            #endregion
+
+            #region Deep Copy
+
+            //int[] Arr01 = { 1, 2, 3 };
+            //int[] Arr02 = new int[3];
+            //Arr02 = (int[])Arr01.Clone();
+            //// in this case Deep Copy Happen
+            // this will take copy from values of Arr02 And Make New instance and make it in the new instance 
+            //after that will make the reference share to that 
+            #endregion
+            #endregion
+
+            #region Shallow Copy And Deep Copy Reference Type
+
+            #region Immutable
+            //string[] Arr01 = { "hesham", "Ahmed" };
+            //string[] Arr02 = new string[2];
+            //// Shallo Copy
+            //Arr02 = Arr01; =>>>> Shallow Copy
+
+
+            //Arr02 = (string[])Arr01.Clone(); =>>>>>>  Deep Copy
+
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
