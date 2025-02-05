3e6b0a13af27b547f512e4608a8d6022f6e39715
Hesham Mathana <heshammathana0@gmail.com>
2025-02-05 18:00:10 +0200

    Apply For Shallow Copy And Deep Copy in Pure

---
+++ Shallow Copy And deep Copy/Shallow Copy And deep Copy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shallow_Copy_And_deep_Copy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
