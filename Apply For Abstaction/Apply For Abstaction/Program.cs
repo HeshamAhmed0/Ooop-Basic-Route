5150c96d8b22cdad45f89aa6262e8c259cbf002f
Hesham Mathana <heshammathana0@gmail.com>
2025-02-07 20:34:38 +0200

    Apply For Abstraction

---
+++ Apply For Abstaction/Apply For Abstaction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Apply_For_Abstaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
