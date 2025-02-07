c63a71249e35f88c4e1ad0f64fffc50ed7e722ec
Hesham Mathana <heshammathana0@gmail.com>
2025-02-07 23:13:28 +0200

    Apply For Static

---
+++ Apply For Static/Apply For Static/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Apply_For_Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //before Make Method Static
+            //Utility U = new Utility(10,20);
+            //Console.WriteLine($"Area Is {U.GetArea()}");
+
+            //After Make method Static
+            Utility.Num1 = 10;
+            Utility.Num2 = 20;
+            Console.WriteLine($"Area Is {Utility.GetArea()}");
+
+        }
+    }
+}
---
+++ Apply For Static/Apply For Static/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apply_For_Static
+{
+    internal class Utility
+    {
+       
+
+        public static int Num1 { get; set; }
+        public static int Num2 { get; set; }
+        
+        public Utility(int num1, int num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public static double GetArea()
+        {
+            return Num1 * Num2;
+        }
+    }
+}
