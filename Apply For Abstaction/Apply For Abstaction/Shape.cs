15f0962ab358ffde5f3d135df8b3dc4e70de20eb
Hesham Mathana <heshammathana0@gmail.com>
2025-02-07 20:41:28 +0200

    UPDATE For Abstraction

--- Apply For Abstaction/Apply For Abstaction/Shape.cs
+++ Apply For Abstaction/Apply For Abstaction/Shape.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Apply_For_Abstaction
 {
-    internal class Shape
+    internal abstract class Shape
     {
+        public decimal Dim01 {  get; set; }
+        public decimal Dim02 { get; set; }
+
+        public abstract decimal GetShape();
+
+        public abstract decimal Perimiter { get;  }
+    }
+
+    class Rectangle : Shape
+    {
+        public override decimal Perimiter
+        {
+            get
+            {
+                return Dim01 + Dim02;
+            }
+        }
+
+        public override decimal GetShape()
+        {
+            return Dim01 * Dim02 * 3.14m;
+        }
     }
 }
