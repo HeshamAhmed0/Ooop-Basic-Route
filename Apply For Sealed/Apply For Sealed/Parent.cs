4a3ae4c2ad11e5d9dfabb926b99efaaab459506f
Hesham Mathana <heshammathana0@gmail.com>
2025-02-08 16:23:22 +0200

    Apply For Sealed

---
+++ Apply For Sealed/Apply For Sealed/Parent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apply_For_Sealed
+{
+    internal class Parent
+    {
+        public virtual void MyFun()
+        {
+            Console.WriteLine("I am Parent");
+        }
+    }
+
+    class Child : Parent
+    {
+        public sealed override void MyFun()
+        {
+            Console.WriteLine("I am Child");
+        }
+    }
+
+    class GrandChild:Child
+    {
+        // Can not override because it sealed
+        //public override void MyFun()
+        //{
+        //    Console.WriteLine("I am GrandChild");
+        //}
+    }
+}
---
+++ Apply For Sealed/Apply For Sealed/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Apply_For_Sealed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
